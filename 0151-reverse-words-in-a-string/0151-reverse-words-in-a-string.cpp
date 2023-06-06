91ce7a43f9b4330f8e3229b8638dcb40ee7f0fea
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-06-06 20:00:54 +0530

    Time: 3 ms (87.8%), Space: 7 MB (92.55%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        int n = s.size();
-        string res = "";
-        string curr = "";
-        for(int i=n-1;i>=0;i--) {
-            if(s[i]==' ') {
-                if(curr.size()==0) continue;
-                if(res.size()!=0) res += " ";
-                reverse(curr.begin(),curr.end());
-                res += curr;
-                curr = "";
-            }else {
-                curr += s[i];
-            }
-        }
-        if(curr.size()==0) return res;
-        if(res.size()!=0) res += " ";
-        reverse(curr.begin(),curr.end());
-        res += curr;
+                                  // O(n) - SC
+        // int n = s.size();
+        // string res = "";
+        // string curr = "";
+        // for(int i=n-1;i>=0;i--) {
+        //     if(s[i]==' ') {
+        //         if(curr.size()==0) continue;
+        //         if(res.size()!=0) res += " ";
+        //         reverse(curr.begin(),curr.end());
+        //         res += curr;
+        //         curr = "";
+        //     }else {
+        //         curr += s[i];
+        //     }
+        // }
+        // if(curr.size()==0) return res;
+        // if(res.size()!=0) res += " ";
+        // reverse(curr.begin(),curr.end());
+        // res += curr;
 
-        return res;
+        // return res;
+
+                            // Space Optimisation O(1)
+
+        reverse(s.begin(),s.end());
+        int i=0,l=0,r=0,n=s.size();
+        while (i < n) {
+            while (i < n && s[i] != ' ')
+                s[r++] = s[i++];
+
+            if (l < r) { // if we can find a non-empty word then
+                reverse(s.begin() + l, s.begin() + r); // reverse current word
+                if (r == n) break;
+                s[r++] = ' '; // set empty space
+                l = r;
+            }
+            ++i; // now i == n or s[i] == ' ', so we skip that character!
+        }
+        if (r > 0 && s[r-1] == ' ') --r; // skip last empty character if have
+        s.resize(r);
+        return s;
     }
 };
