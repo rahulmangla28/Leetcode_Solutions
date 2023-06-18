575a2375ff45ab4cb390a1cd85b102539801e6ef
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-06-18 14:58:31 +0530

    Time: 28 ms (49.55%), Space: 19.2 MB (76.44%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n = arr.size();
+        int ind = 0;
+        for(int i=0;i<n;i++) {
+            if(arr[i]==0) ind++;
+            else if(ind>0) {
+                arr[i-ind] = arr[i];
+                arr[i] = 0;
+            }
+        }
+    }
+};
