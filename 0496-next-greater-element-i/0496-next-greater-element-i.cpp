199eb931b4e58f2746409bb2265fab93708d6c5c
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-06-18 15:33:59 +0530

    Time: 11 ms (37.95%), Space: 8.8 MB (54.78%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> st;
-        vector<int> v;
-        for(int i=0;i<nums1.size();i++){
-            for(int j=nums2.size()-1;j>=0;j--) st.push(nums2[j]);
-                while(v.size()!=(i+1)){
-                    if(st.empty()) v.push_back(-1);
-                    else if(!st.empty()&& st.top()==nums1[i]){
-                        st.pop();
-                        while(!st.empty()){
-                        if(st.top()>nums1[i]){
-                            v.push_back(st.top());
-                            break;
-                        }
-                            st.pop();
-                        }
-                                    }
-                    else st.pop();
-                }
-            while(!st.empty()) st.pop();
+                        // BRUTE APPROACH
+    // vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    //     stack<int> st;
+    //     vector<int> v;
+    //     for(int i=0;i<nums1.size();i++){
+    //         for(int j=nums2.size()-1;j>=0;j--) st.push(nums2[j]);
+    //             while(v.size()!=(i+1)){
+    //                 if(st.empty()) v.push_back(-1);
+    //                 else if(!st.empty()&& st.top()==nums1[i]){
+    //                     st.pop();
+    //                     while(!st.empty()){
+    //                     if(st.top()>nums1[i]){
+    //                         v.push_back(st.top());
+    //                         break;
+    //                     }
+    //                         st.pop();
+    //                     }
+    //                                 }
+    //                 else st.pop();
+    //             }
+    //         while(!st.empty()) st.pop();
+    //     }
+    //     return v;
+    // }
+
+                    // OPTIMISED APPROACH
+    vector<int> nextGreaterElement(vector<int>& findNums, vector<int>& nums) {
+        stack<int> s;
+        unordered_map<int, int> m;
+        for (int n : nums) {
+            while (s.size() && s.top() < n) {
+                m[s.top()] = n;
+                s.pop();
+            }
+            s.push(n);
         }
-        return v;
+        vector<int> ans;
+        for (int n : findNums) ans.push_back(m.count(n) ? m[n] : -1);
+        return ans;
     }
 };
