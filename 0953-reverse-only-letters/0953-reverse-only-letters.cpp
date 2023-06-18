c64b3d438bb40b5b3678c27383e8945bf0e2832b
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-06-18 19:35:55 +0530

    Time: 4 ms (31.79%), Space: 6 MB (90.53%) - LeetHub

---
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0,j=s.size()-1;
+        while(i<=j){
+            if(((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122)) && ((s[j]>=65 && s[j]<=90) || (s[j]>=97 && s[j]<=122))){
+                swap(s[i],s[j]);
+                i++;
+                j--;
+            }
+            else if(((s[i]>=65 && s[i]<=90) || (s[i]>=97 && s[i]<=122)) && ((s[j]>=33 && s[j]<=64) || (s[j]>=91 && s[j]<=96))) j--;
+            else i++;
+        }
+        return s;
+    }
+};
+
+           // SOLUTION
+           // reverse from backward side and store in a string
