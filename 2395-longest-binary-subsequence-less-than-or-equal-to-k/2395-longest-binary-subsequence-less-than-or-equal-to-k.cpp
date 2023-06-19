cfa75729f15c339705cf88fa1926e758d4d798d0
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-06-20 01:27:47 +0530

    Time: 6 ms (46.56%), Space: 7.3 MB (20.24%) - LeetHub

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(string &s,int index,int power,long long score,int& k,vector<int>& dp){
+        if(index<0)return 0;
+        if(dp[index]!=-1)return dp[index];
+        
+        int include=0;
+        if(s[index]=='1'){
+            if(score+pow(2,power)<=k){
+                include=1+solve(s,index-1,power+1,score+ pow(2,power),k,dp);
+            }
+        }else{
+            include=1+solve(s,index-1,power+1,score,k,dp);
+        }
+        int notinclude=solve(s,index-1,power,score,k,dp);
+        return dp[index]=max(include,notinclude);
+    }
+    int longestSubsequence(string s, int k) {
+        int n=s.length();
+        vector<int> dp(n+1,-1);
+        int power=0;
+        long long score=0;
+        return solve(s,n-1,power,score,k,dp);
+    }
+};
