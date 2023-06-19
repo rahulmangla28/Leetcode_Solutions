fe9ee213cb5d4078da4948950d662e3189257367
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-06-19 23:53:40 +0530

    Time: 581 ms (13.32%), Space: 102.5 MB (12.27%) - LeetHub

---
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -0,0 +1,24 @@
+bool comp(vector<int> &a,vector<int> &b) {
+	return a[1]<b[1];
+}
+class Solution {
+public:
+	int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+		int ans=-1;      
+		if(intervals.size()==0) return 0;       
+		sort(intervals.begin(),intervals.end(),comp);      //custom comperator is used.
+		vector<int> prev= intervals[0];
+
+		for(vector<int> i: intervals) {
+			if(prev[1]>i[0]) {
+                //we dont update previous, because i[1] will be grater then prev[1]
+				ans++;   
+			}else {
+                // we want the end point to be minimum
+                prev=i;          
+            }
+		}
+        //ans was initially made -1 because our prev and intervals[0] will always match 
+		return ans;                 
+	}
+};
