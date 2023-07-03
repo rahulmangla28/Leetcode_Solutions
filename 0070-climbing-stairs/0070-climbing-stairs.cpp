8724b58d944fbd1260358e2e33bf06b4443f73b0
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-07-03 11:18:11 +0530

    Time: 0 ms (100%), Space: 6.2 MB (36.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,10 +5,12 @@ public:
         vector<int> dp(n+1);
         dp[0] = 1;
         dp[1] = 1;
-
+        int prev=1,prev2=1;
         for(int i=2;i<=n;i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
