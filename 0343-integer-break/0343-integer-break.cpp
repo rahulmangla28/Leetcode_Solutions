510fe26e14795bf3c6a487c1f0d237f58724a451
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-08-24 19:30:20 +0530

    Time: 2 ms (49.21%), Space: 6.4 MB (14.05%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1,0);
+        if(n<=2) return 1;
+        dp[1] = 0;
+        dp[2] = 1;
+
+        for(int i=3;i<=n;i++) {
+            for(int j = 1; j < i ;j++) {
+                int res = max(j*(i-j), j*dp[i-j]);
+                dp[i] = max(dp[i], res);
+            }
+        }
+
+        return dp[n];
+    }
+};
