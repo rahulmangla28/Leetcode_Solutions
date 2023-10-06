c233d6b9bedb8c8a6d57d87ab08726fbf889522d
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-10-06 11:35:31 +0530

    Time: 0 ms (100%), Space: 6.3 MB (36.38%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
+    // int integerBreak(int n) {
+    //     vector<int> dp(n+1,0);
+    //     if(n<=2) return 1;
+    //     dp[1] = 0;
+    //     dp[2] = 1;
+
+    //     for(int i=3;i<=n;i++) {
+    //         for(int j = 1; j < i ;j++) {
+    //             int res = max(j*(i-j), j*dp[i-j]);
+    //             dp[i] = max(dp[i], res);
+    //         }
+    //     }
+
+    //     return dp[n];
+    // }
+
+                               // Alternate
+
     int integerBreak(int n) {
-        vector<int> dp(n+1,0);
-        if(n<=2) return 1;
-        dp[1] = 0;
-        dp[2] = 1;
-
-        for(int i=3;i<=n;i++) {
-            for(int j = 1; j < i ;j++) {
-                int res = max(j*(i-j), j*dp[i-j]);
-                dp[i] = max(dp[i], res);
-            }
+        if(n==2) return 1;
+        if(n==3) return 2;
+        int product = 1;
+        while(n>4){
+            product*=3;
+            n-=3;
         }
-
-        return dp[n];
-    }
+        product*=n;
+        
+        return product;
+    }    
 };
