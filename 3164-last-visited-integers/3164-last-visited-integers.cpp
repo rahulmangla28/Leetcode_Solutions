7299b810a72c150e74ca201fff54638a4c2f6bad
Rahul Mangla <93324315+rahulmangla28@users.noreply.github.com>
2023-10-15 03:15:54 +0530

    Time: 11 ms (40%), Space: 23.8 MB (20%) - LeetHub

--- 3164-last-visited-integers/3164-last-visited-integers.cpp
+++ 3164-last-visited-integers/3164-last-visited-integers.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     vector<int> lastVisitedIntegers(vector<string>& words) {
-        int n = words.size();
-        vector<int> res;
-        vector<int> vis;
-        int cnt = 0;
-        for(int i=0;i<n;i++) {
-            if(words[i] != "prev") {
-                vis.push_back(stoi(words[i]));
-                cnt = 0;
-            }else {
-                cnt += 1;
-                reverse(vis.begin(),vis.end());
-                if(cnt > vis.size()) res.push_back(-1);
-                else res.push_back(vis[cnt-1]);
-                reverse(vis.begin(),vis.end());
+        vector<int>v;
+        vector<int>ans;
+        int count=0;
+        for(int i=0;i<words.size();i++){
+            if(words[i]=="prev"){
+                count++;
+                if(count>v.size()){
+                    ans.push_back(-1);
+                }
+                else{
+                    ans.push_back(v[v.size()-count]);
+                }
+            }
+            else{
+                count=0;
+                v.push_back(stoi(words[i]));
             }
         }
-        return res;
+        return ans;
     }
 };
